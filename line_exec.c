d71dab4a324b4e7eb20958e34d8f873ab6f491b9
Atamba-Chinaza <110666732+Atamba-Chinaza@users.noreply.github.com>
2022-11-16 13:49:06 +0100

    Update line_exec.c

--- line_exec.c
+++ line_exec.c
@@ -1,91 +1,137 @@
 #include "shell.h"
 
+
 /**
- * _realloc - reallocates memory block
- * @ptr: pointer to the previous memory
- * @old_size: the old size
- * @new_size: the new size
- *
- * Return: a pointer to the newly allocated memory
+ * splitstring - splits a string and makes it an array of pointers to words
+ * @str: the string to be split
+ * @delim: the delimiter
+ * Return: array of pointers to words
  */
+
+char **splitstring(char *str, const char *delim)
+{
+	int i, wn;
+	char **array;
+	char *token;
+	char *copy;
+
+	copy = malloc(_strlen(str) + 1);
+	if (copy == NULL)
+	{
+		perror(_getenv("_"));
+		return (NULL);
+	}
+	i = 0;
+	while (str[i])
+	{
+		copy[i] = str[i];
+		i++;
+	}
+	copy[i] = '\0';
+
+	token = strtok(copy, delim);
+	array = malloc((sizeof(char *) * 2));
+	array[0] = _strdup(token);
+
+	i = 1;
+	wn = 3;
+	while (token)
+	{
+		token = strtok(NULL, delim);
+		array = _realloc(array, (sizeof(char *) * (wn - 1)), (sizeof(char *) * wn));
+		array[i] = _strdup(token);
+		i++;
+		wn++;
+	}
+	free(copy);
+	return (array);
+}
+
+/**
+ * execute - executes a command
+ * @argv: array of arguments
+ */
+
+void execute(char **argv)
+{
+
+	int d, status;
+
+	if (!argv || !argv[0])
+		return;
+	d = fork();
+	if (d == -1)
+	{
+		perror(_getenv("_"));
+	}
+	if (d == 0)
+	{
+		execve(argv[0], argv, environ);
+			perror(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	wait(&status);
+}
+
+/**
+ * _realloc - Reallocates memory block
+ * @ptr: previous pointer
+ * @old_size: old size of previous pointer
+ * @new_size: new size for our pointer
+ * Return: New resized Pointer
+ */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *result;
+	char *new;
+	char *old;
+
+	unsigned int i;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
 
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0 && ptr)
+
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	result = malloc(new_size);
-	if (result == NULL)
+
+	new = malloc(new_size);
+	old = ptr;
+	if (new == NULL)
 		return (NULL);
-	if (ptr == NULL)
+
+	if (new_size > old_size)
 	{
-		fill_an_array(result, '\0', new_size);
+		for (i = 0; i < old_size; i++)
+			new[i] = old[i];
+		free(ptr);
+		for (i = old_size; i < new_size; i++)
+			new[i] = '\0';
+	}
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			new[i] = old[i];
 		free(ptr);
 	}
-	else
-	{
-		_memcpy(result, ptr, old_size);
-		free(ptr);
-	}
-	return (result);
+	return (new);
 }
-/**
- * _memset - fills a memory with constant byte
- * @s: pointer to memory area
- * @n: first n bytes
- * @byt: constant byte
- *
- * Return: A pointer to a character
- */
-char *_memset(char *s, char byt, unsigned int n)
-{
-	unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = byt;
-	}
-	return (s);
-}
 /**
- * free_data - frees data
- * @data: the data structure
- *
- * Return: (Success) positive number
- * ------- (Fail) negative number
+ * freearv - frees the array of pointers arv
+ *@arv: array of pointers
  */
-int free_data(sh_t *data)
-{
-	free(data->line);
-	data->line = NULL;
-	free(data->args);
-	data->args = NULL;
-	free(data->cmd);
-	data->cmd = NULL;
-	free(data->error_msg);
-	data->error_msg = NULL;
-	return (0);
-}
-/**
- * _memcpy - cpies memory area
- * @dest: Destination memory area
- * @src: Source memory area
- * @n: Amount of memory byte
- *
- * Return: A pointer to dest
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+void freearv(char **arv)
+{
+	int i;
+
+	for (i = 0; arv[i]; i++)
+		free(arv[i]);
+	free(arv);
 }
