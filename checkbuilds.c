62f5310695875aaeff8bf1eb6c4e7587d3af2748
Atamba-Chinaza <atambanazzy@gmail.com>
2022-11-16 04:28:21 -0800

    Task 1

--- checkbuilds.c
+++ checkbuilds.c
@@ -1,77 +1,34 @@
 #include "shell.h"
-/**
- * is_path_form - chekc if the given fikenname is a path
- * @data: the data strucct pointer
- *
- * Return: (Success)
- * ------- (Fail) otherwise
- */
-int is_path_form(sh_t *data)
-{
-	if (_strchr(data->args[0], '/') != 0)
-	{
-		data->cmd = _strdup(data->args[0]);
-		return (SUCCESS);
-	}
-	return (FAIL);
-}
-#define DELIMITER ":"
-/**
- * is_short_form - chekc if the given fikenname is short form
- * @data: the data strucct pointer
- *
- * Return: (Success)
- * ------- (Fail) otherwise
- */
-void is_short_form(sh_t *data)
-{
-	char *path, *token, *_path;
-	struct stat st;
-	int exist_flag = 0;
 
-	path = _getenv("PATH");
-	_path = _strdup(path);
-	token = strtok(_path, DELIMITER);
-	while (token)
-	{
-		data->cmd = _strcat(token, data->args[0]);
-		if (stat(data->cmd, &st) == 0)
-		{
-			exist_flag += 1;
-			break;
-		}
-		free(data->cmd);
-		token = strtok(NULL, DELIMITER);
-	}
-	if (exist_flag == 0)
-	{
-		data->cmd = _strdup(data->args[0]);
-	}
-	free(_path);
-}
-#undef DELIMITER
 /**
- * is_builtin - checks if the command is builtin
- * @data: a pointer to the data structure
- *
- * Return: (Success) 0 is returned
- * ------- (Fail) negative number will returned
- */
-int is_builtin(sh_t *data)
+* checkbuild - checks if the command is a buildin
+* @arv: array of arguments
+* Return: pointer to function that takes arv and returns void
+*/
+void(*checkbuild(char **arv))(char **arv)
 {
-	blt_t blt[] = {
-		{"exit", abort_prg},
-		{"cd", change_dir},
-		{"help", display_help},
+	int i, j;
+	mybuild T[] = {
+		{"exit", exitt},
+		{"env", env},
+		{"setenv", _setenv},
+		{"unsetenv", _unsetenv},
 		{NULL, NULL}
 	};
-	int i = 0;
 
-	while ((blt + i)->cmd)
+	for (i = 0; T[i].name; i++)
 	{
-		if (_strcmp(data->args[0], (blt + i)->cmd) == 0)
-			return (SUCCESS);
-		i++;
+		j = 0;
+		if (T[i].name[j] == arv[0][j])
+		{
+			for (j = 0; arv[0][j]; j++)
+			{
+				if (T[i].name[j] != arv[0][j])
+					break;
+			}
+			if (!arv[0][j])
+				return (T[i].func);
+		}
 	}
-	return (NEUTRAL);
+	return (0);
 }
